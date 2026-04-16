ba005befe9fb65d34e0fd0abfd7fe39a01de9dff
Sandesh2061 <sandeshp2061@gmail.com>
2026-04-16 20:39:42 +0930

    Refactor evaluator.py for improved readability

--- Question 2/evaluator.py
+++ Question 2/evaluator.py
@@ -18,7 +18,7 @@ def tokenize (expr):
       while i<len(expr):
             ch = expr[i]
             if ch.isspace():
-                  i += o
+                  i += 1
                   continue
             if ch.isdigit() or ch=='.':
                   start = i
